2ac7cb3e4f8b32bd67274f3a0f86bc13c0420489
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2018-10-22 15:40:14 +0200

    Filename typo

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,5 @@
 mkdir testrun
-for fn in a b c d; do [ -e testrun/${fn}.mzML ] || cp testdata/fr08_veryminimal.mzML testrun/${fn}.mzML; done
+for fn in a b c d; do [ -e testrun/${fn}.mzML ] || cp testdata/few_scans.mzML testrun/${fn}.mzML; done
 echo `pwd`/testrun/a.mzML'\t'setA'\t'37-49'\t'08 > testrun/mzmldef.txt
 echo `pwd`/testrun/b.mzML'\t'setA'\t'37-49'\t'09 >> testrun/mzmldef.txt
 echo `pwd`/testrun/c.mzML'\t'setB'\t'37-49'\t'08 >> testrun/mzmldef.txt
