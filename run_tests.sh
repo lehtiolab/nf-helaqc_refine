88b3a1b8ddc8b8f1f03a5c6ecb75c7c634c5d8a7
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2024-04-23 14:18:01 +0200

    Added tests

---
+++ run_tests.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+rundir=$(pwd)
+export repodir=$(dirname "$(realpath -s "$0")")
+export testdir="${repodir}/tests/"
+export testdata="${rundir}/static-resources/test-data/ddamsproteomics"
+
+if [ -e "${testdata}" ]
+then
+    cd "${testdata}" && git checkout ddamsproteomics_test_data && cd "${rundir}"
+else
+    git clone --single-branch --branch ddamsproteomics_test_data https://github.com/lehtiolab/static-resources
+fi
+
+
+export NXF_VER=23.10.1
+
+nextflow run -resume -profile docker "${repodir}/refine_mzml.nf" \
+	--input <(cat "${repodir}/test/refine_mzml.txt" | envsubst) \
+	--db "${testdata}/lf.fa" \
+	--instrument qe
